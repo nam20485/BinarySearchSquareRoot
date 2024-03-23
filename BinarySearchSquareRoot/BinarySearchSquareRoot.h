ad851022df1d26b34aa58a49f2560c5d5905852f
Nathan Miller <nmiller217@gmail.com>
2024-03-23 15:30:06 -0700

    Add project files.

---
+++ BinarySearchSquareRoot/BinarySearchSquareRoot.h
@@ -0,0 +1,46 @@
+﻿// BinarySearchSquareRoot.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <cmath>
+
+static double truncate(double d, double precision)
+{
+	return static_cast<int>(d / precision) * precision;
+}
+
+static float bssqrt(double d)
+{
+	const double precision = 0.0001;
+
+	double left = 0.0, right = d;
+
+	while (true)
+	{
+		double mid = left + (right - left) / 2;
+		double midsquared = mid * mid;
+		if (std::abs(midsquared - d) <= precision)
+		{
+			// found answer within +- precision,
+			// now throw away everything after "level of precision"
+			auto val = mid * (1.0 / precision);
+			auto rounded = std::round(val);
+			auto m = rounded * precision;
+			return (float)m;
+			return static_cast<int>(m / precision) * precision;
+
+		}
+		else if (midsquared > d)
+		{
+			right = mid;
+		}
+		else if (midsquared < d)
+		{
+			left = mid;
+		}
+	}
+
+	// unreachable
+	return -1.0;
+}
---
+++ BinarySearchSquareRoot/main.cpp
@@ -0,0 +1,23 @@
+﻿// BinarySearchSquareRoot.cpp : Defines the entry point for the application.
+//
+
+#include "BinarySearchSquareRoot.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	auto sr = bssqrt(4);
+
+	auto sr1 = bssqrt(9);
+
+	auto sr2 = bssqrt(100);
+
+	auto sr3 = bssqrt(103);
+
+	auto sr4 = bssqrt(13);
+
+	cout << "Hello CMake." << endl;
+	return 0;
+}
